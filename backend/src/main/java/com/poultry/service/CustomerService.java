cf73d354e231b922eeab964aa156d9c38c2f7ce0
phaneendra <phaneendrajava3@gmail.com>
2025-09-01 09:40:47 +0530

    Initial commit

---
+++ backend/src/main/java/com/poultry/service/CustomerService.java
@@ -0,0 +1,9 @@
+
+package com.poultry.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    // Business logic will go here
+}
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,3 @@
+import { Component } from '@angular/core';
+@Component({ selector: 'app-root', template: '<h1>Poultry App</h1>' })
+export class AppComponent {}
